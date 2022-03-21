df5473dfbaaff9c5afd92529b2659c31ee928327
JohnnyBas1 <82596466+JohnnyBas1@users.noreply.github.com>
2022-03-20 23:57:02 -0400

    added game url

--- urls.py
+++ urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.logged_in, name="login"),
     path("signout", views.sign_out, name="signout"),
-    path("playlists", views.playlist_page, name="playlists")
+    path("playlists", views.playlist_page, name="playlists"),
+    path("game", views.game, name="game")
 ]

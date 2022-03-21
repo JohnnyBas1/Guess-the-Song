558db1fc9cb71848014da67f10112c8ade8a5e11
JohnnyBas1 <82596466+JohnnyBas1@users.noreply.github.com>
2022-03-20 23:56:39 -0400

    updated with game view and post method!

--- views.py
+++ views.py
@@ -1,9 +1,11 @@
+from abc import get_cache_token
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import json
+
 
 # Create your views here.
 
@@ -15,7 +17,7 @@ def index(request):
         request=request)
 
     auth_manager = spotipy.oauth2.SpotifyOAuth(
-        client_id="CLIENT_ID", client_secret="CLIENT_SECRET", redirect_uri="REDIRECT_URI", scope=scope, cache_handler=cache_handler)
+        client_id="b4500f1dc0064d498aeabc56c140ead0", client_secret="c3b0d3f0e2864dce93deeb5def7f4e9c", redirect_uri="http://localhost:8000/guess_song_1/", scope=scope, cache_handler=cache_handler)
 
     # code is the return verified authentication from spotify server!!! you're getting this from the url and then erasing it by doing redirect(index)
     # so that the end user does not see the token.
@@ -63,6 +65,7 @@ def sign_out(request):
 
 
 def playlist_page(request):
+
     cache_handler = spotipy.cache_handler.DjangoSessionCacheHandler(
         request=request)
     auth_manager = spotipy.oauth2.SpotifyOAuth(
@@ -78,7 +81,31 @@ def playlist_page(request):
     # [items][n][name] to get all the names!
     # pretty = json.dumps(
     #    spotify_playlists['items'][10]['name'], skipkeys=False, indent=0)
+    dataJSON = json.dumps(spotify_playlists['items'])
 
     return render(request, "guess_song_1/playlists.html", {
-        "playlists": spotify_playlists
+        "playlists": spotify_playlists,
+        "playlistsJSON": dataJSON
     })
+
+
+def game(request):
+    if request.method == 'POST':
+        playlistNumber = int(request.POST['playlist'])
+        cache_handler = spotipy.cache_handler.DjangoSessionCacheHandler(
+            request=request)
+        auth_manager = spotipy.oauth2.SpotifyOAuth(
+            client_id="b4500f1dc0064d498aeabc56c140ead0", client_secret="c3b0d3f0e2864dce93deeb5def7f4e9c", redirect_uri="http://localhost:8000/guess_song_1", cache_handler=cache_handler)
+
+        if not auth_manager.validate_token(cache_handler.get_cached_token()):
+            return redirect('/')
+
+        spotify = spotipy.Spotify(auth_manager=auth_manager)
+
+        spotify_playlists = spotify.current_user_playlists()
+
+        return render(request, "guess_song_1/game.html", {
+            "playlist": spotify_playlists['items'][playlistNumber],
+            "token": cache_handler.get_cached_token()
+
+        })
